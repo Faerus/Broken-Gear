943cf4095af14141db575b5a7eb9006a0e1f340f
Faerus <Tyalis@gmail.com>
2022-05-24 22:52:00 +0200

    Test commit 2

--- Assets/Scripts/Library/Movement/RandomoveOnGrid.cs
+++ Assets/Scripts/Library/Movement/RandomoveOnGrid.cs
@@ -29,7 +29,7 @@ public class RandomoveOnGrid : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (this.Move.IsAtTargetPosition()) // test
+        if (this.Move.IsAtTargetPosition())
         {
             // Arrived at cell destination
             this.Cell = this.TargetCell;
